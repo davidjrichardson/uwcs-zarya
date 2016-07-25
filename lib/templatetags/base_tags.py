de80aa22f4505a9379820b289207e64e6e52fb58
David Richardson <david@tankski.co.uk>
2016-07-25 13:29:27 +0100

    Add common project templates

---
+++ lib/templatetags/base_tags.py
@@ -0,0 +1,40 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    return context['request'].site.root_page
+
+
+# Retrieves the top menu items
+@register.inclusion_tag('lib/tags/top_menu.html', takes_context=True)
+def top_menu(context, parent, calling_page=None):
+    menuitems = parent.get_children().live().in_menu()
+    for menuitem in menuitems:
+        # We don't directly check if calling_page is None since the template
+        # engine can pass an empty string to calling_page
+        # if the variable passed as calling_page does not exist.
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        'is_home': calling_page.url == u'/',
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
+
+# Retrieves the footer sitemap items
+@register.inclusion_tag('lib/tags/footer.html', takes_context=True)
+def footer(context, parent):
+    return {
+        'menuitems': parent.get_children().live().in_menu(),
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
