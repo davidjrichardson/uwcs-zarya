60d71f0f6dc9de01442c304978ee7966319a5049
David Richardson <david@tankski.co.uk>
2016-09-20 14:54:41 +0100

    Change database connections for data migration tool

--- zarya/settings/dev.py
+++ zarya/settings/dev.py
@@ -8,9 +8,22 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'l33th4x0rs'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'uwcs_zarya',
+        'USER': 'uwcs_zarya',
+        'PASSWORD': 'doyouevenlift',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+    'old_data': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 try:
     from .local import *
