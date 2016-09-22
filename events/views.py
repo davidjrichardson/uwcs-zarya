6e3656d2b4193bcc4ba4d562fb3748b02f1dfcc5
David Richardson <david@tankski.co.uk>
2016-09-22 22:39:38 +0100

    Add templates for signup edge-cases

--- events/views.py
+++ events/views.py
@@ -9,12 +9,15 @@ class EventSignupView(LoginRequiredMixin, View):
     login_url = '/accounts/login/'
     redirect_field_name = 'redirect_to'
     template_name = 'events/event_signup.html'
+    already_done_template = 'events/event_already_signedup.html'
 
     def post(self, request, event_id):
         event = get_object_or_404(EventPage, page_ptr_id=event_id)
 
         if EventSignup.objects.filter(event=event, member=request.user).first():
-            return redirect('/')  # TODO: Page for users that are already signed up
+            return render(request, self.already_done_template, {
+                'event': event
+            })
 
         signup = EventSignup(member=request.user, event=event, comment=request.POST.get('comment', ''))
         signup.save()
@@ -28,6 +31,7 @@ class EventUnsignupView(LoginRequiredMixin, View):
     login_url = '/accounts/login/'
     redirect_field_name = 'redirect_to'
     template_name = 'events/event_unsignup.html'
+    already_done_template = 'events/event_already_unsignedup.html'
 
     def get(self, request, event_id):
         event = get_object_or_404(EventPage, page_ptr_id=event_id)
@@ -35,7 +39,9 @@ class EventUnsignupView(LoginRequiredMixin, View):
         signup = EventSignup.objects.filter(event=event, member=request.user).first()
 
         if not signup:
-            return redirect('/')  # TODO: Page for users that aren't signed up
+            return render(request, self.already_done_template, {
+                'event': event
+            })
         else:
             signup.delete()
 
