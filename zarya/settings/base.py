fca4df27943ccc843983ef8a968894264dc03c1e
David Richardson <david@tankski.co.uk>
2016-10-10 12:35:40 +0100

    Fix stylesheet issues

--- zarya/settings/base.py
+++ zarya/settings/base.py
@@ -142,7 +142,7 @@ PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 BOWER_COMPONENTS_ROOT = os.path.join(PROJECT_PATH, "../components")
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss',
-     'sass --scss -I "%s/bower_components/foundation-sites/scss" -I "%s/bower_components/motion-ui" "{infile}" "{outfile}"' % (
+     'sass --scss --style compressed -I "%s/bower_components/foundation-sites/scss" -I "%s/bower_components/motion-ui" "{infile}" "{outfile}"' % (
      BOWER_COMPONENTS_ROOT, BOWER_COMPONENTS_ROOT)),
 )
 
