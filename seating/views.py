76a2c3133f39f66c051b10a59b08742b512ba0e1
David Richardson <david@tankski.co.uk>
2017-07-02 13:44:04 +0100

    Start work on seating plan

---
+++ seating/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View, RedirectView
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+
+# TODO
