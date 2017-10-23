e4a40307f94bcb8ff2e0361d41371a3891b259b8
David Richardson <david@tankski.co.uk>
2017-10-23 15:35:21 +0100

    Fix issue introduced w/ previous patch

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -34,32 +34,17 @@ def mail_newsletter(recipients, mail):
     email.send()
 
 
-def queryset_iterator(queryset, chunk_size=1000):
-    """
-    Iterate over a Django Queryset ordered by the primary key
-    This method loads a maximum of chunk_size (default: 1000) rows in it's
-    memory at the same time while django normally would load all rows in it's
-    memory. Using the iterator() method only causes it to not preload all the
-    classes.
-    Note that the implementation of the iterator does not support ordered query sets.
-    """
-    try:
-        last_pk = queryset.order_by('-pk')[:1].get().pk
-    except ObjectDoesNotExist:
-        return
-
-    pk = 0
-    queryset = queryset.order_by('pk')
-    while pk < last_pk:
-        for row in queryset.filter(pk__gt=pk)[:chunk_size]:
-            pk = row.pk
-            yield row
-        gc.collect()
+# Create a function called "chunks" with two arguments, l and n:
+def chunks(l, n):
+    # For item i in a range that is a length of l,
+    for i in range(0, len(l), n):
+        # Create an index range for l of n items:
+        yield l[i:i+n]
 
 
 @task(name='send_newsletter')
 def send_newsletter(mail_id):
-    subscriptions = queryset_iterator(Subscription.objects.all(), chunk_size=500)
+    subscriptions = chunks(Subscription.objects.all(), 500)
 
     for chunk in subscriptions:
         mail_newsletter(chunk, Mail.objects.get(id=mail_id))
