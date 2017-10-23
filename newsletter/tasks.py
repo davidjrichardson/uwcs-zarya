1ebd6ba05ea16b65eadec5a77b4a7a6fc11d4313
David Richardson <david@tankski.co.uk>
2017-10-23 14:19:05 +0100

    Fix issue with SendGrid supporting 1000 recipients per message

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -1,5 +1,7 @@
+import gc
 from celery.decorators import task
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.urls import reverse
@@ -32,6 +34,32 @@ def mail_newsletter(recipients, mail):
     email.send()
 
 
+def queryset_iterator(queryset, chunk_size=1000):
+    """
+    Iterate over a Django Queryset ordered by the primary key
+    This method loads a maximum of chunk_size (default: 1000) rows in it's
+    memory at the same time while django normally would load all rows in it's
+    memory. Using the iterator() method only causes it to not preload all the
+    classes.
+    Note that the implementation of the iterator does not support ordered query sets.
+    """
+    try:
+        last_pk = queryset.order_by('-pk')[:1].get().pk
+    except ObjectDoesNotExist:
+        return
+
+    pk = 0
+    queryset = queryset.order_by('pk')
+    while pk < last_pk:
+        for row in queryset.filter(pk__gt=pk)[:chunk_size]:
+            pk = row.pk
+            yield row
+        gc.collect()
+
+
 @task(name='send_newsletter')
 def send_newsletter(mail_id):
-    mail_newsletter(Subscription.objects.all(), Mail.objects.get(id=mail_id))
+    subscriptions = queryset_iterator(Subscription.objects.all(), chunk_size=500)
+
+    for chunk in subscriptions:
+        mail_newsletter(chunk, Mail.objects.get(id=mail_id))
