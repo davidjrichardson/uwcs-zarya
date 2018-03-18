3bae8e4a7cb1848c1beba94caa28c05ca886ea9e
David Richardson <david@tankski.co.uk>
2018-03-18 14:49:38 +0000

    Restructure user discord info API

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url('^discord/(?P<uni_id>[0-9]+)/$', views.MemberDiscordInfoApiView.as_view(), name='discord_user_view'),
+    # url('^events/$') # TODO: Create events list
+    # url('^events/signup/(?P<event_id>[0-9]+)/$') # TODO: Create events signup endpoint
+    url('^user/(?P<uni_id>[0-9]+)/$', views.MemberDiscordInfoApiView.as_view(), name='discord_user_view'),
 ]
--- zarya/settings/base.py
+++ zarya/settings/base.py
@@ -74,7 +74,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
